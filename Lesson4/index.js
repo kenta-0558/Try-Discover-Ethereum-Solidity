dc63b8b53cf0198948009c70bf177d1c04eacc22
kenta0558 <mannheim42@yahoo.de>
2021-03-30 23:22:02 +0200

    update and finish Lesson4

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const solc = require('solc');
+const Web3 = require('web3');
 
 const content = fs.readFileSync('HelloWorld.sol', 'utf-8');
 
@@ -21,4 +22,55 @@ const input = {
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-console.log(output);
+const provider = new Web3.providers.HttpProvider("http://localhost:8545");
+
+const web3 = new Web3(provider);
+
+const { HelloWorld } = output.contracts["HelloWorld.sol"];
+const { abi, evm } = HelloWorld;
+
+const contract = new web3.eth.Contract(abi);
+
+console.log(contract);
+
+const deployAndRunContract = async () => {
+
+    const addresses = await web3.eth.getAccounts();
+    const gasPrice = await web3.eth.getGasPrice();
+    console.log(addresses, gasPrice);
+
+    contract.deploy({
+        data: evm.bytecode.object,
+    })
+    .send({
+        from: addresses[0],
+        gas: 1000000,
+        gasPrice,
+    })
+    .on('confirmation', async (confNumber, receipt) => {
+        console.log(confNumber, receipt);
+        // const { contractAddress } = receipt
+        // console.log("Deployed at", contractAddress);
+    
+        // // const contractInstance = new web3.eth.Contract(abi, contractAddress)
+    
+        // // const myName = await contractInstance.methods.getMyName().call();
+        // // console.log("Result from blockchain:", myName);
+    })
+    .on('confirmation', async (confNumber, receipt) => {
+        console.log(confNumber, receipt);
+        const { contractAddress } = receipt;
+        console.log("Deploy at", contractAddress);
+
+        const contractInstance = new web3.eth.Contract(abi, contractAddress);
+
+        const myName = await contractInstance.methods.getMyName().call();
+        console.log("Success", myName);
+    })
+    .on('error', (error) => {
+        console.log("failed", error);
+    })
+
+}
+
+deployAndRunContract();
