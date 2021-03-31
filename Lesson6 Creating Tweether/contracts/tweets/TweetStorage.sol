0eafbcd2d31eead7d43eb6f4abefd4acd2e5278a
kenta0558 <mannheim42@yahoo.de>
2021-03-31 23:20:36 +0200

    create new contract TweetStorage and new integrration test for it

---
+++ Lesson6 Creating Tweether/contracts/tweets/TweetStorage.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TweetStorage {
+
+    uint latestTweetId = 0;
+
+    struct Tweet {
+        uint id;
+        string text;
+        uint userId;
+        uint postedAt;        
+    }
+
+    mapping (uint => Tweet) public tweets;
+
+    function createTweet(uint _userId, string memory _text) public returns (uint) {
+
+        latestTweetId++;
+
+        tweets[latestTweetId] = Tweet(latestTweetId, _text, _userId, block.timestamp);
+
+        return latestTweetId;
+    }
+
+    // function getTweet() {
+
+    // }
+
+}
---
+++ Lesson6 Creating Tweether/test/unit/TestTweetStorage.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../../contracts/tweets/TweetStorage.sol";
+
+
+contract TestTweetStorage {
+
+    function testCreateTweet() public {
+
+        TweetStorage _storage = TweetStorage(DeployedAddresses.TweetStorage());
+
+        uint _userId = 1;
+        uint _expectedTweetId = 1;
+
+        Assert.equal(
+            _storage.createTweet(_userId, "I will be a blockchain developer."),
+            _expectedTweetId,
+            "ID of created tweet must be 1"
+        );
+
+    } 
+}
+
